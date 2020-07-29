94dd0dd71ea6e28e3fdaca5794ed3be9e1f91c19
Mateus Fugita <mateushiwao@gmail.com>
2020-07-29 00:05:04 -0300

    create routes

---
+++ src/pages/cadastro/Categoria/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+
+export default function CadastroCategoria(){
+    return(
+        <PageDefault>
+            <h1>Cadastro de Categoria</h1>
+
+            <form>
+                <label>Nome da Categoria
+                    <input type='text' />
+                </label>
+                <button>
+                    Cadastrar
+                </button>
+            </form>
+
+            <Link to='/'>
+                Ir para Home
+            </Link>
+        </PageDefault>
+    )
+}
