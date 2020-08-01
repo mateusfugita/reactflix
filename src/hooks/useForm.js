95d80269ba466824c268412d30ceaf3f0d4e16ad
Mateus Fugita <mateushiwao@gmail.com>
2020-08-01 20:25:34 -0300

    create custom hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export default function useForm(valoresIniciais) {
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(key, value) {
+    setValues({
+      ...values,
+      [key]: value,
+    });
+  }
+
+  function handleChange(e) {
+    setValue(e.target.getAttribute('name'), e.target.value);
+  }
+
+  function clearForm() {
+    setValues(valoresIniciais);
+  }
+
+  return {
+    values,
+    handleChange,
+    clearForm,
+  };
+}
