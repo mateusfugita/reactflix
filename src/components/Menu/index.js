93b3cbde723031f5608d40c028a5fa5ff4942b21
Mateus Fugita <mateushiwao@gmail.com>
2020-08-01 20:29:54 -0300

    set admin prop

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,18 +3,19 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 import './styles.css';
 import Button from '../Button';
-// import ButtonLink from './components/ButtonLink';
 
-export default function Menu(){
-    return(
-        <nav className='Menu'>
-            <Link to='/'>
-                <img className='Logo' src={logo} alt='FugitaFlix'></img>
-            </Link>
+export default function Menu({ isAdmin }) {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={logo} alt="FugitaFlix" />
+      </Link>
 
-            <Button as={Link} className='ButtonLink' to='/cadastro/video'>
-                Novo vídeo
-            </Button>
-        </nav>
-    )
+      {isAdmin && (
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+      )}
+    </nav>
+  );
 }
